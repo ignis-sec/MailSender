e9ffbcf917885a6889f8ad42b827031b0e90b3af
Ata Hakçıl <ahakcil@gmail.com>
2018-04-08 14:30:13 +0300

    Added title to arguements and added mass mail to list or single mail option

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ metuSmtpPort = 587
 metuSmtpServer = "smtp.metu.edu.tr"
 gmailSmtpPort = 587
 gmailSmtpServer = "smtp.gmail.com"
+devsSmtpPort = 587
+devsSmtpServer = "smtp.mail.com"
 
 
 def message_parse(fileName):
@@ -21,10 +23,10 @@ def html_parse(fileName):
 
     return html
 
-def session(uid, pwd, fromAddr, toAddr,isHtml):
+def session(uid, pwd, fromAddr, toAddr,isHtml, subject):
     # msg object has to be recreated in every new session
     msg = message_parse("message.txt")
-    msg['Subject'] = "HelloWorld"
+    msg['Subject'] = subject
     msg['From'] = fromAddr
     msg['To'] = toAddr
     if(isHtml):
@@ -33,7 +35,7 @@ def session(uid, pwd, fromAddr, toAddr,isHtml):
         msg.attach(html)
 
 
-    smtpObject = smtplib.SMTP(metuSmtpServer, metuSmtpPort)
+    smtpObject = smtplib.SMTP(devsSmtpServer, devsSmtpPort)
     smtpObject.ehlo()
     smtpObject.starttls()
     resp = smtpObject.login(uid, pwd)
@@ -48,17 +50,26 @@ def session(uid, pwd, fromAddr, toAddr,isHtml):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("--recepient", help="recepient address")
     parser.add_argument("uid", help="User id --which will be e+first 5 digits of school number--")
     parser.add_argument("pwd", help="User password ")
     parser.add_argument("fromAddr", help="Sender's email")
+    parser.add_argument("--title", help="Mail Subject")
+    
 
-    args = parser.parse_args()
+    args = parser.parse_args() 
     uid = args.uid
     pwd = args.pwd
     fromAddr = args.fromAddr
-
-
-    with open("emails.txt", "r") as file:
-        for item in file:
-            session(uid, pwd, fromAddr, item,0)
-        file.close()
+    if args.title:
+        title = args.title
+    else:
+        title = "Untitled Email"
+        
+    if args.recepient:
+        session(uid, pwd, fromAddr, args.recepient,0, title)
+    else:
+        with open("emails.list", "r") as file:
+            for item in file:
+                session(uid, pwd, fromAddr, item,0, title)
+            file.close()
